ad8b4087dee888012c42c867bddb0f48881ada84
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-22 03:08:54 -0700

    done

--- main.lua
+++ main.lua
@@ -1,13 +1,5 @@
-local grade = tonumber(io.read())
+local n = tonumber(io.read())
 
-if grade >= 90 then
-	print("A")
-elseif grade >= 80 then
-	print("B")
-elseif grade >= 70 then
-	print("C")
-elseif grade >= 60 then
-	print("D")
-else
-	print("F")
+for i = 1, 10 do
+	print(string.format("%d x %d = %d", n, i, n * i))
 end
