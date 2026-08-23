0f09eca90e28a730510cefeb234ccf87103c4b14
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-23 02:31:42 -0700

    done

--- main.lua
+++ main.lua
@@ -1,22 +1,19 @@
-local function stepper(t, k)
-	local step = 1
-	return function()
-		local index = step * k
-		if index <= #t then
-			step = step + 1
-			return index, t[index]
+local defaults = { colour = "black", size = "1" }
+local config = setmetatable({}, { __index = defaults })
+
+local count = tonumber(io.read())
+for _ = 1, count do
+	local command, key, val = io.read():match("^(%u+)%s+(%a+)%s*(.*)$")
+
+	if command == "GET" then
+		local stored_val = config[key]
+		if not stored_val then
+			print(key .. "=none")
+		else
+			local ownership = rawget(config, key) and "own" or "default"
+			print(key .. "=" .. stored_val .. " (" .. ownership .. ")")
 		end
+	elseif command == "SET" then
+		config[key] = val
 	end
-end
-
-local n, k = io.read():match("(%d+)%s+(%d+)")
-n, k = tonumber(n), tonumber(k)
-
-local t = {}
-for i = 1, n do
-	t[i] = io.read()
-end
-
-for i, v in stepper(t, k) do
-	print(i .. ":" .. v)
 end
