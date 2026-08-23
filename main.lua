667ca84ec583cbb5c04e34f721186b799cb672e1
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-22 23:20:58 -0700

    done

--- main.lua
+++ main.lua
@@ -1,16 +1,22 @@
-local words = io.read()
-
-local distinct = {}
-for w in string.gmatch(words, "%S+") do
-	distinct[w] = (distinct[w] or 0) + 1
+local function stepper(t, k)
+	local step = 1
+	return function()
+		local index = step * k
+		if index <= #t then
+			step = step + 1
+			return index, t[index]
+		end
+	end
 end
 
-local sorted = {}
-for k, _ in pairs(distinct) do
-	table.insert(sorted, k)
-end
-table.sort(sorted)
+local n, k = io.read():match("(%d+)%s+(%d+)")
+n, k = tonumber(n), tonumber(k)
 
-for _, k in ipairs(sorted) do
-	print(k .. ": " .. distinct[k])
+local t = {}
+for i = 1, n do
+	t[i] = io.read()
+end
+
+for i, v in stepper(t, k) do
+	print(i .. ":" .. v)
 end
