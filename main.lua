f64fa31663af448db5014f91f7a995975ce22a95
MrBanh <25971310+MrBanh@users.noreply.github.com>
2026-08-26 16:21:04 -0700

    done

--- main.lua
+++ main.lua
@@ -1,37 +1,20 @@
-local Stack = {}
-Stack.__index = Stack
+local function minmax(t)
+	local min, max = nil, nil
 
-function Stack.new()
-	return setmetatable({}, Stack)
+	for _, v in ipairs(t) do
+		min = (min and min < v and min) or v
+		max = (max and max > v and max) or v
+	end
+
+	return min, max
 end
-
-function Stack:push(v)
-	table.insert(self, v)
-end
-
-function Stack:pop()
-	return table.remove(self, #self)
-end
-
-function Stack:size()
-	return #self
-end
-
-local stacks = { a = Stack.new(), b = Stack.new() }
 
 local n = tonumber(io.read())
-
+local nums = {}
 for _ = 1, n do
-	local line = io.read() or ""
-	local cmd, name, value = line:match("^(%u+)%s+(%a+)%s*(.*)$")
-	local stack = stacks[name]
-	if cmd == "PUSH" then
-		stack:push(value)
-	elseif cmd == "POP" then
-		local popped = stack:pop()
-		print(popped or "empty")
-	elseif cmd == "SIZE" then
-		local size = stack:size()
-		print(size)
-	end
+	local input = tonumber(io.read())
+	table.insert(nums, input)
 end
+
+local min, max = minmax(nums)
+print(min .. " " .. max)
